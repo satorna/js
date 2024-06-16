90d08018aca4a4378ebb96aef12fbeb78e167b98
satorna <152053199+satorna@users.noreply.github.com>
2024-06-16 17:09:41 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function start(){
+	document.getElementById("bgaudio").play();
+	var t = 0;
+	var d = setInterval(()=>{
+		t++;
+		document.getElementById("click_to_start").style.opacity = 1 - 0.01 * t;
+		if(t > 100){
+			document.getElementById("click_to_start").style.display = "none";
+			clearInterval(d);
+		}
+	},20);
+}
+document.oncontextmenu = () => false;
